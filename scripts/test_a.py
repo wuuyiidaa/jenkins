6d8b08c931f3f87d88ea279142bd27b4903f4f0f
admin <123456>
2019-01-25 17:40:28 +0800

    aa

---
+++ scripts/test_a.py
@@ -0,0 +1,14 @@
+class Test_allure:
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_al(self):
+        print('aaaa')
+        assert 0
+
+    def test_bl(self):
+        print('bbbb')
+        assert 1
